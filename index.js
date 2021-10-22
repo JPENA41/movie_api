cfd243e5432ecd87ecfa3a540036b97b3e26ec3a
JPENA41 <jpenavillalba7348@gmail.com>
2021-10-21 18:55:48 -0700

    Create the route for the requests,
    1. I added three get request that returns the movie by title, the movie by genre, and the director's name.
    
    2. I added two post requests that allow registration and adds a movie to the list of favorites.
    
    3. I added two delete request that removes a favorite movie from the user's favorites and unregisters an existing user.
    
    4. I included a comment that says "listen for requests" and another comment that says "Get a list of every movie."

--- index.js
+++ index.js
@@ -61,6 +61,8 @@ let topTenMovies = [
     }
 ];
 
+app.use(morgan('common'));
+
 app.get('/', (req, res) => {
     res.send('Welcome to myFlix App!');
 });
@@ -69,14 +71,49 @@ app.get('/documentation', (req, res) => {
 	res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-// Get the list of all movies
+// Get a list of every movie
 app.get('/movies', (req, res) => {
     res.json(topTenMovies);
 });
 
-// Get data of a director's name
-app.get('/directors/:name', (req, res) => {
-    res.send('Welcome to the directors information.');
+// Get a particular movie by title.
+app.get('/movies/:title', (req, res) => {
+    res.send('Successful GET request retuning movie by title');
+});
+
+// Get data about a specific movie genre
+app.get('/genres/:genre', (req, res) => {
+    res.send('Successful GET request retuning movie by genre');
+});
+
+// Get data about a director by name
+app.get('/directors/:directorsName', (req, res) => {
+    res.send('Successful GET request of directors information.');
+});
+
+// Allow registation
+app.post('/register', (req, res) => {
+    res.send('Successful POST to the server a new user registeration.');
+});
+
+// Allow user to update their username
+app.put('/users/:id/:infoToUpdate/:newValue', (req, res) => {
+    res.send('Successful PUT to the server a user information.');
+});
+
+// Allow user to add a movie to the list of favorites
+app.post('/users/:id/:favorites/:newFavorite', (req, res) => {
+    res.send('Successful POST to the server a favorite movie on the user list.');
+});
+
+// Remove a favorite movie from the user's favorites
+app.delete('/users/:id/favorites/:deleteFavorite', (req, res) => {
+    res.send('Successful DELETE to the server a favorite movie on the user list.');
+});
+
+// Unregister a existing user from the database
+app.delete('/users/:id/unregister', (req, res) => {
+    res.send('Successful DELETE a user!');
 });
 
 // Error handling
@@ -84,6 +121,8 @@ app.use((err, req, res,next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
+
+// Listen for requests
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
