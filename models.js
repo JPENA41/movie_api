439e76c8fc085bf3d358b8932de289edbd2ef576
JPENA41 <jpenavillalba7348@gmail.com>
2021-10-28 15:12:52 -0700

    Define director & genre schema & exporting the modules

--- models.js
+++ models.js
@@ -23,13 +23,32 @@ let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
   Password: {type: String, required: true},
   Email: {type: String, required: true},
+  Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectID, ref: 'Movie' }]
 });
 
-// Create collections called “db.movies” and “db.users”
+//  Define the “schema" for documents in the "Genre" collection
+let genreSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Description: {type: String, required: true}
+})
+
+//  Define the “schema" for documents in the "Director" collection
+let directorSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Bio: {type: String, required: true},
+    Birth: {type: Date, required: true},
+    Death: {type: String}
+});
+
+// Created collections
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
+let Genre = mongoose.model('Genre', genreSchema);
+let Director = mongoose.model('Director', directorSchema);
 
-// Export elements to import them onto “index.js” file
+// Export modules onto “index.js” file
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Genre = Genre;
+module.exports.Director = Director;
