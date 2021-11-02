5816771c366d4a5c602401248ac39a23dec7a8d3
JPENA41 <jpenavillalba7348@gmail.com>
2021-11-02 13:25:05 -0700

    Removed genre and director schema along the export modules

--- models.js
+++ models.js
@@ -27,28 +27,10 @@ let userSchema = mongoose.Schema({
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectID, ref: 'Movie' }]
 });
 
-//  Define the “schema" for documents in the "Genre" collection
-let genreSchema = mongoose.Schema({
-    Name: {type: String, required: true},
-    Description: {type: String, required: true}
-})
-
-//  Define the “schema" for documents in the "Director" collection
-let directorSchema = mongoose.Schema({
-    Name: {type: String, required: true},
-    Bio: {type: String, required: true},
-    Birth: {type: Date, required: true},
-    Death: {type: String}
-});
-
 // Created collections
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
-let Genre = mongoose.model('Genre', genreSchema);
-let Director = mongoose.model('Director', directorSchema);
 
 // Export modules onto “index.js” file
 module.exports.Movie = Movie;
 module.exports.User = User;
-module.exports.Genre = Genre;
-module.exports.Director = Director;
