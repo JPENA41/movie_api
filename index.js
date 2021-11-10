f292808590fc865663c175e365e8400caeff8b2f
JPENA41 <jpenavillalba7348@gmail.com>
2021-11-09 20:43:56 -0700

    Temporarily remove the authentication middleware

--- index.js
+++ index.js
@@ -47,14 +47,14 @@ app.get('/', (req, res) => {
 });
 
 // Get a list of every movie
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get("/movies", function (req, res) {
   Movies.find()
-    .then((movies) => {
+    .then(function (movies) {
       res.status(201).json(movies);
     })
-    .catch((error) => {
+    .catch(function (error) {
       console.error(error);
-      res.status(500).send('Error: ' + error);
+      res.status(500).send("Error: " + error);
     });
 });
 
